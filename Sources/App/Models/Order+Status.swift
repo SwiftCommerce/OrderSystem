12fd9e1d1e086970eb489189fd3479fe977790d0
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-05-15 18:20:11 -0500

    Conformed Order.Status enum to MySQLDataConvertible and MySQLColumnDefinitionStaticRepresentable protocols

--- Sources/App/Models/Order+Status.swift
+++ Sources/App/Models/Order+Status.swift
@@ -5,3 +5,29 @@ extension Order {
         case open, processing, closed, cancelled
     }
 }
+
+extension Order.Status: MySQLDataConvertible, MySQLColumnDefinitionStaticRepresentable {
+    static var mySQLColumnDefinition: MySQLColumnDefinition { return .tinyInt() }
+    
+    func convertToMySQLData() throws -> MySQLData {
+        return .init(integer: self.rawValue)
+    }
+    
+    static func convertFromMySQLData(_ mysqlData: MySQLData) throws -> Order.Status {
+        guard let rawValue = try mysqlData.integer(Int.self) else {
+            throw FluentError(
+                identifier: "badDataType",
+                reason: "Connot create `Order.Status` instance from non-`Int` type",
+                source: .capture()
+            )
+        }
+        guard let instance = Order.Status(rawValue: rawValue) else {
+            throw FluentError(
+                identifier: "badRawValue",
+                reason: "Coannot create `Order.Status` from value '\(rawValue)'",
+                source: .capture()
+            )
+        }
+        return instance
+    }
+}
