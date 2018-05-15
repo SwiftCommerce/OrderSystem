4d5312b9a2a9719db23e7fd8de5378f01db6b938
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-05-15 18:08:22 -0500

    Added Order model to migrations config

--- Sources/App/Configuration/configure.swift
+++ Sources/App/Configuration/configure.swift
@@ -27,6 +27,7 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     /// Configure migrations
     var migrations = MigrationConfig()
     migrations.add(migration: Item.self, database: .mysql)
+    migrations.add(migration: Order.self, database: .mysql)
     services.register(migrations)
 
 }
