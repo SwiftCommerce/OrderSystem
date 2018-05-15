254a0e0b0d25da25001e018aa1c4e521bb5bb9a6
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-05-15 16:33:19 -0500

    Installed FluentMySQL package

--- Package.swift
+++ Package.swift
@@ -4,11 +4,11 @@ import PackageDescription
 let package = Package(
     name: "OrderSystem",
     dependencies: [
-        // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0")
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
+        .package(url: "https://github.com/vapor/fluent-mysql.git", from: "3.0.0-rc")
     ],
     targets: [
-        .target(name: "App", dependencies: ["Vapor"]),
+        .target(name: "App", dependencies: ["Vapor", "FluentMySQL"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
