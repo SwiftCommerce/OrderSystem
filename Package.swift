e6f32a0d971c7fd1b11bf9f8992a6899bc33b6a7
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-05-15 16:27:49 -0500

    Removed example code

--- Package.swift
+++ Package.swift
@@ -5,13 +5,10 @@ let package = Package(
     name: "OrderSystem",
     dependencies: [
         // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
-
-        // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
-        .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0-rc.2")
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0")
     ],
     targets: [
-        .target(name: "App", dependencies: ["FluentSQLite", "Vapor"]),
+        .target(name: "App", dependencies: ["Vapor"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
