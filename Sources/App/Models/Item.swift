44040440bf3c5c76c17e039f3709f1100b0cfd85
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-05-15 17:50:20 -0500

    Created Item model with default 'tax' value

--- Sources/App/Models/Item.swift
+++ Sources/App/Models/Item.swift
@@ -1,6 +1,29 @@
+import Foundation
 import FluentMySQL
 import Vapor
 
-final class Item: Content, MySQLModel, Migration {
+final class Item: Codable, Content, MySQLModel, Migration {
     var id: Int?
+
+    let orderID: Int
+    let sku: String
+    var price: Int
+    var quantity: Int
+    
+    var tax: Int { print("TODO: Set `Item.tax` property the correct way"); return 108 }
+    var total: Int { return price * quantity }
+    var totalWithTax: Int { return (total * tax) / 100 }
+    
+    init(from decoder: Decoder)throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.id = try container.decodeIfPresent(Int.self, forKey: .id)
+        self.orderID = try container.decode(Int.self, forKey: .orderID)
+        self.sku = try container.decode(String.self, forKey: .sku)
+        self.price = try container.decode(Int.self, forKey: .price)
+        self.quantity = try container.decode(Int.self, forKey: .quantity)
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case id, orderID, sku, price, quantity
+    }
 }
