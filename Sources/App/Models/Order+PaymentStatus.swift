b54ec0cfe838031dd8ed99623239383b694c8050
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-05-16 15:20:12 -0500

    Replaced Order.PaymentStatus MySQLDataConvertible and MySQLColumnDefinitionStaticRepresentable conformance with MySQLEnumType

--- Sources/App/Models/Order+PaymentStatus.swift
+++ Sources/App/Models/Order+PaymentStatus.swift
@@ -1,25 +1,7 @@
 import FluentMySQL
 
 extension Order {
-    enum PaymentStatus: Int, Codable {
+    enum PaymentStatus: Int, Codable, MySQLEnumType {
         case open, partial, paid, refunded
-    }
-}
-
-extension Order.PaymentStatus: MySQLDataConvertible, MySQLColumnDefinitionStaticRepresentable {
-    static var mySQLColumnDefinition: MySQLColumnDefinition = .tinyInt()
-    
-    func convertToMySQLData() throws -> MySQLData {
-        return .init(integer: self.rawValue)
-    }
-    
-    static func convertFromMySQLData(_ mysqlData: MySQLData) throws -> Order.PaymentStatus {
-        guard let rawValue = try mysqlData.integer(Int.self) else {
-            throw FluentError(identifier: "badDataType", reason: "Cannot create `Order.PaymentStatus` instance from non-`Int` type", source: .capture())
-        }
-        guard let instance = Order.PaymentStatus(rawValue: rawValue) else {
-            throw FluentError(identifier: "badRawValue", reason: "Cannot create `Order.PaymentStatus` from value '\(rawValue)'", source: .capture())
-        }
-        return instance
     }
 }
