75a6d957184ae3e910978762fbfb362417e1e082
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-05-16 13:42:52 -0500

    Removed init(from:) decoder initializer method from AccountSetting model

--- Sources/App/Models/AccountSetting.swift
+++ Sources/App/Models/AccountSetting.swift
@@ -14,14 +14,6 @@ final class AccountSetting: Content, MySQLModel, Migration, Parameter {
         self.value = value
     }
     
-    init(from decoder: Decoder)throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        self.id = try container.decodeIfPresent(Int.self, forKey: .id)
-        self.accountID = try container.decode(Account.ID.self, forKey: .accountID)
-        self.name = try container.decode(String.self, forKey: .name)
-        self.value = try container.decode(String.self, forKey: .value)
-    }
-    
     public static func prepare(on connection: MySQLConnection) -> Future<Void> {
         return Database.create(self, on: connection) { builder in
             try addProperties(to: builder)
