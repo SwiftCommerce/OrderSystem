0ad3ba957635d8687ea14a49990985ffaeffc272
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-05-16 07:18:25 -0500

    Created cusotom preparation for AccountSetting model that adds a foreign key foir the .accountID property

--- Sources/App/Models/AccountSetting.swift
+++ Sources/App/Models/AccountSetting.swift
@@ -7,4 +7,11 @@ final class AccountSetting: Content, MySQLModel, Migration {
     let accountID: Account.ID
     let name: String
     var value: String
+    
+    public static func prepare(on connection: MySQLConnection) -> Future<Void> {
+        return Database.create(self, on: connection) { builder in
+            try addProperties(to: builder)
+            try builder.addReference(from: \.accountID, to: \Account.id)
+        }
+    }
 }
