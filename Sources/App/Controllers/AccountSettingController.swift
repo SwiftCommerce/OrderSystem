3c32ba7254814276818ff7d18e162d532c830e34
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-05-16 11:11:25 -0500

    Created AccountSettingController.create handler and AccountSettingContent struct

--- Sources/App/Controllers/AccountSettingController.swift
+++ Sources/App/Controllers/AccountSettingController.swift
@@ -3,4 +3,16 @@ import Vapor
 
 final class AccountSettingController: RouteCollection {
     func boot(router: Router) throws {}
+    
+    func create(_ request: Request, _ setting: AccountSettingContent)throws -> Future<AccountSetting> {
+        let accountID = try request.parameters.next(Account.ID.self)
+        return AccountSetting(account: accountID, name: setting.name, value: setting.value).save(on: request)
+    }
+}
+
+struct AccountSettingContent: Content {
+    let id: AccountSetting.ID?
+    let accountID: Account.ID?
+    let name: String
+    let value: String
 }
