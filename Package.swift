257c8890e84493fbdc96ff026602a6a348d996fa
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-05-16 12:08:42 -0500

    Installed JWTMiddleware package

--- Package.swift
+++ Package.swift
@@ -5,10 +5,11 @@ let package = Package(
     name: "OrderSystem",
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
-        .package(url: "https://github.com/vapor/fluent-mysql.git", from: "3.0.0-rc")
+        .package(url: "https://github.com/vapor/fluent-mysql.git", from: "3.0.0-rc"),
+        .package(url: "https://github.com/skelpo/JWTMiddleware.git", from: "0.6.1")
     ],
     targets: [
-        .target(name: "App", dependencies: ["Vapor", "FluentMySQL"]),
+        .target(name: "App", dependencies: ["Vapor", "FluentMySQL", "JWTMiddleware"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
