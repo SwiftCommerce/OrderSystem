0173a24c2ba369a10d5c978345d5a125a51b583e
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-05-16 10:54:30 -0500

    Registred AccountSettingController with main router

--- Sources/App/Configuration/routes.swift
+++ Sources/App/Configuration/routes.swift
@@ -1,4 +1,6 @@
 import Vapor
 
 /// Register your application's routes here.
-public func routes(_ router: Router) throws {}
+public func routes(_ router: Router) throws {
+    try router.register(collection: AccountSettingController())
+}
