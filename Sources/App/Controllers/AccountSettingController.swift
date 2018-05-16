02818bfe7ad4ca761ef270e6b70998f173c580f0
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-05-16 11:18:32 -0500

    Created AccountSettingController.get handler

--- Sources/App/Controllers/AccountSettingController.swift
+++ Sources/App/Controllers/AccountSettingController.swift
@@ -13,6 +13,18 @@ final class AccountSettingController: RouteCollection {
         let accountID = try request.parameters.next(Account.ID.self)
         return try AccountSetting.query(on: request).filter(\.accountID == accountID).all()
     }
+    
+    func get(_ request: Request)throws -> Future<AccountSetting> {
+        let accountID = try request.parameters.next(Account.ID.self)
+        let settingID = try request.parameters.next(AccountSetting.ID.self)
+        
+        return try Account.query(on: request).filter(\.id == accountID).count().flatMap(to: AccountSetting?.self) { count in
+            guard count > 0 else {
+                throw Abort(.notFound, reason: "No account found with ID '\(accountID)'")
+            }
+            return try AccountSetting.query(on: request).filter(\.accountID == accountID).filter(\.id == settingID).first()
+        }.unwrap(or: Abort(.notFound, reason: "No account setting found with ID '\(settingID)' for account '\(accountID)'"))
+    }
 }
 
 struct AccountSettingContent: Content {
