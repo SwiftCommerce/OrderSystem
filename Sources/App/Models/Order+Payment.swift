046ae468af10b945e68d1baa4d24418ea6bef629
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-10-15 11:02:47 -0500

    Created Order.Payment.currency property

--- Sources/App/Models/Order+Payment.swift
+++ Sources/App/Models/Order+Payment.swift
@@ -8,14 +8,16 @@ extension Order {
         let orderID: Order.ID
         let paymentMethod: String
         var externalID: String?
+        var currency: String
         var paidTotal: Int
         var refundedTotal: Int
         
-        init(orderID: Order.ID, paymentMethod: String, paidTotal: Int, refundedTotal: Int) {
+        init(orderID: Order.ID, paymentMethod: String, currency: String, paidTotal: Int, refundedTotal: Int) {
             self.orderID = orderID
             self.paymentMethod = paymentMethod
             self.paidTotal = paidTotal
             self.refundedTotal = refundedTotal
+            self.currency = currency
         }
         
         public static func prepare(on connection: MySQLConnection) -> Future<Void> {
