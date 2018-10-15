904df07038f56bfb9c653748f491bc3daa884a6a
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-10-15 11:31:21 -0500

    Added Order.Payment.total property formatting if currency type is PayPal.Currency

--- Sources/App/Payment/PayPal.swift
+++ Sources/App/Payment/PayPal.swift
@@ -4,12 +4,35 @@ import PayPal
 typealias PayPalPayment = TransactionPayPal.PayPalPayment<Order, Order.Payment>
 typealias PayPalController = PaymentController<PayPalPayment>
 
-extension Order.Payment: ExecutablePayment {
-    var currency: Currency {
-        return .usd
+extension Int {
+    func formatted(for currency: CurrencyProtocol) -> String {
+        let exponent: Int
+        if let currency = currency as? Currency {
+            exponent = currency.e ?? 0
+        } else {
+            exponent = 0
+        }
+
+        var string = String(describing: self)
+        
+        if exponent == 0 {
+            return string
+        }
+        if string.count > exponent {
+            string.insert(".", at: string.index(string.endIndex, offsetBy: -exponent))
+        } else {
+            return "0." + String(repeating: "0", count: exponent - string.count) + string
+        }
+        return string
     }
-    
+}
+
+extension Order.Payment: ExecutablePayment {
     var total: String {
-        return String(describing: self.paidTotal)
+        if let paypalCurrency = PayPal.Currency(code: self.currency) {
+            return self.paidTotal.formatted(for: paypalCurrency)
+        } else {
+            return String(describing: self.paidTotal)
+        }
     }
 }
