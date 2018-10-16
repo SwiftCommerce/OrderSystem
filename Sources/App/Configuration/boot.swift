419850fc715a77daf7bb56f3ebb947a87e80b5c0
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-10-16 10:45:10 -0500

    Create test JWT in development environment on boot

--- Sources/App/Configuration/boot.swift
+++ Sources/App/Configuration/boot.swift
@@ -1,4 +1,17 @@
+import JWTVapor
 import Vapor
 
 /// Called after your application has initialized.
-public func boot(_ app: Application) throws {}
+public func boot(_ app: Application) throws {
+    
+    // If in a development environment, create and log a JWT token to use for testing the service with.
+    if !app.environment.isRelease {
+        let signer = try app.make(JWTService.self)
+        
+        let now = Date().timeIntervalSince1970
+        let token = User(exp: now + 3_600, iat: now, email: "token@example.com", id: Int.random(in: 0...Int.max))
+        let jwt = try signer.sign(token)
+        
+        print("Auth Token:", jwt)
+    }
+}
