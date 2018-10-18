24b29b8d7bae699a5f337b47f63a8c59d76b412a
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-10-18 11:55:29 -0500

    Added .subtotal, .shipping, .handling, .shippingDiscount, .insurence, and .giftWrap properties to Order.Payment model

--- Sources/App/Models/Order+Payment.swift
+++ Sources/App/Models/Order+Payment.swift
@@ -2,23 +2,47 @@ import FluentMySQL
 import Vapor
 
 extension Order {
-    final class Payment: Content, MySQLModel, Migration, Parameter {
+    final class Payment {
         var id: Int?
         
-        let orderID: Order.ID
         let paymentMethod: String
+        let orderID: Order.ID
         var externalID: String?
         var currency: String
-        var paidTotal: Int
-        var refundedTotal: Int
         
-        init(orderID: Order.ID, paymentMethod: String, currency: String, paidTotal: Int, refundedTotal: Int) {
+        var paid: Int
+        var refunded: Int
+        
+        var subtotal: Int
+        var shipping: Int?
+        var handling: Int?
+        var shippingDiscount: Int?
+        var insurence: Int?
+        var giftWrap: Int?
+        
+        init(orderID: Order.ID, paymentMethod: String, currency: String, subtotal: Int, paid: Int, refunded: Int) {
             self.orderID = orderID
             self.paymentMethod = paymentMethod
-            self.paidTotal = paidTotal
-            self.refundedTotal = refundedTotal
+            self.subtotal = subtotal
+            self.paid = paid
+            self.refunded = refunded
             self.currency = currency
         }
+    }
+}
+
+extension Order.Payment: Migration {
+    static func prepare(on connection: MySQLConnection) -> Future<Void> {
+        return Database.create(self, on: connection) { builder in
+            try addProperties(to: builder)
+            builder.reference(from: \.orderID, to: \Order.id)
+        }
+    }
+}
+
+extension Order.Payment: Content {}
+extension Order.Payment: Parameter {}
+extension Order.Payment: MySQLModel {}        
         
         public static func prepare(on connection: MySQLConnection) -> Future<Void> {
             return Database.create(self, on: connection) { builder in
