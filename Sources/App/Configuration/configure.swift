508a725f76d03977bdfda8118e41c37f7b7c54a9
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-10-18 16:19:11 -0500

    Added PayPalPayment and StripeCC payment method services to app services

--- Sources/App/Configuration/configure.swift
+++ Sources/App/Configuration/configure.swift
@@ -8,22 +8,26 @@ import PayPal
 
 /// Called before your application initializes.
 public func configure(_ config: inout Config, _ env: inout Vapor.Environment, _ services: inout Services) throws {
+    
     /// Register providers first
     try services.register(FluentMySQLProvider())
     try services.register(TransactionProvider())
-    try services.register(StripeProvider())
-    try services.register(PayPalProvider())
     try services.register(JWTProvider { n, d in
         let headers = JWTHeader(alg: "RS256", crit: ["exp", "aud"])
         return try RSAService(n: n, e: "AQAB", d: d, header: headers)
     })
+    
+    try services.register(StripeProvider())
+    try services.register(PayPalProvider())
 
+    
     /// Register routes to the router
     let router = EngineRouter.default()
     try routes(router)
     services.register(router, as: Router.self)
     services.register(OrderService())
 
+    
     /// Register middleware
     var middlewares = MiddlewareConfig() // Create _empty_ middleware config
     middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
@@ -38,11 +42,13 @@ public func configure(_ config: inout Config, _ env: inout Vapor.Environment, _ 
     try databases(config: &databaseConfig)
     services.register(databaseConfig)
 
+    
     /// Configure migrations
     var migrationConfig = MigrationConfig()
     try migrations(config: &migrationConfig)
     services.register(migrationConfig)
 
+    
     /// Configure controllers for making payments with third-party payment providers (i.e. PayPal or Stripe).
     var controllers = PaymentControllers(root: any, "orders")
     controllers.add(PayPalController(structure: .separate))
@@ -50,5 +56,8 @@ public func configure(_ config: inout Config, _ env: inout Vapor.Environment, _ 
     services.register(controllers)
     
     services.register(GlobalConfig.self)
+    
     services.register(Storage())
+    services.register(PayPalPayment.self)
+    services.register(StripeCC.self)
 }
