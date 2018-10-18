30767d97226cf61a0364280dfccf8adf6a78722d
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-10-18 12:01:13 -0500

    Reimplemented Order.Payment.total to add up payment fees and subtotal

--- Sources/App/Payment/PayPal.swift
+++ Sources/App/Payment/PayPal.swift
@@ -6,6 +6,7 @@ typealias PayPalController = PaymentController<PayPalPayment>
 
 extension Order.Payment: ExecutablePayment {
     var total: Int {
-        return self.paidTotal
+        let fees: Int? = self.shipping + self.handling + self.shippingDiscount + self.insurence + self.giftWrap
+        return self.subtotal + fees ?? self.subtotal
     }
 }
