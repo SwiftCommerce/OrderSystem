ae565cb727382d792bdd008f59a2fba54cf93061
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-11-05 12:14:26 -0600

    Added .userID property to Account model

--- Sources/App/Models/Account.swift
+++ Sources/App/Models/Account.swift
@@ -3,4 +3,9 @@ import Vapor
 
 final class Account: Content, MySQLModel, Migration, Parameter {
     var id: Int?
+    let userID: User.ID
+    
+    init(userID: User.ID) {
+        self.userID = userID
+    }
 }
