265ed5714d1ca44a19b0039bbd8c4c89d4102df6
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-02-11 16:28:11 -0600

    Expect expiration claim to fail in UserTests.testExpireClaimFail test case

--- Tests/AppTests/Models/UserTests.swift
+++ Tests/AppTests/Models/UserTests.swift
@@ -3,16 +3,18 @@ import JWTVapor
 @testable import App
 
 final class UserTests: XCTestCase {
-    func testExpireClaim()throws {
-        let iat = Date(timeInterval: -3601, since: Date()).timeIntervalSince1970
-        let exp = Date(timeInterval: -1, since: Date()).timeIntervalSince1970
+    func testExpireClaimFail()throws {
+        let iat = Date(timeInterval: -3600, since: Date()).timeIntervalSince1970
+        let exp = Date().timeIntervalSince1970
         let user = User(exp: exp, iat: iat, email: "guest@example.com", id: -1, status: .admin)
         
-        let signer = JWTSigner.hs256(key: "weak-key")
-        try XCTAssertNoThrow(user.verify(using: signer))
+        do {
+            try user.verify(using: JWTSigner.hs256(key: "weak-key"))
+            XCTFail("User verification should have failed")
+        } catch { /* Success */ }
     }
     
     static let allTests: [(String, (UserTests) -> ()throws -> ())] = [
-        ("testExpireClaim", testExpireClaim)
+        ("testExpireClaimFail", testExpireClaimFail)
     ]
 }
