76e73f5bc7228d06934b89b0a506d9b344a909ee
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-02-11 16:11:00 -0600

    Changed Product.currentPrice(for:) method to sort valid prices by greatest .activeFrom value then greatest .id

--- Sources/App/Models/Product.swift
+++ Sources/App/Models/Product.swift
@@ -8,14 +8,14 @@ struct Product: Content {
     let prices: [Price]?
     
     func currenctPrice(for currency: String) -> Price? {
+        guard self.prices != nil else { return nil }
+        
         let now = Date()
         let validPrices = self.prices?.filter { price in
             return price.active && price.currency.lowercased() == currency.lowercased() && price.activeFrom <= now && price.activeTo > now
-        }.sorted { first, second in
-            return first.activeFrom > second.activeFrom
-        }
+        }.sorted { $0.activeFrom > $1.activeFrom }
         
-        return validPrices?.first
+        return validPrices?.filter { $0.activeFrom == validPrices?.first?.activeFrom }.min { ($0.id ?? 0) > ($1.id ?? 0) }
     }
 }
 
